89a1f2f41c58c7c370447ab8e48c8028085dc5a4
Juan Marin <juanmarin@Juans-MacBook-Pro.local>
2025-02-03 22:59:41 -0700

    modified nav layouts

---
+++ src/pages/MainFooter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MainFooter=()=>{
+    return (
+        <footer>
+            <p>&copy;{new Date().getFullYear}My Website</p>
+
+        </footer>
+    );
+};
+
+export default MainFooter;
---
+++ src/pages/MainNav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MainNav = () => {
+    return (
+        <nav>
+            <h1>My Website</h1>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default MainNav;
